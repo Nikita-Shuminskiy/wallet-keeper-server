cf377358550bb5493f9051e2d0567709f66aa991
Nick <shuminskiy23@gmail.com>
2022-12-24 15:19:18 +0300

    fix chart data

--- src/chart/chart.controller.ts
+++ src/chart/chart.controller.ts
@@ -1,7 +1,7 @@
 import {Controller, Get, HttpException, HttpStatus, Query, UseGuards} from '@nestjs/common';
 import {SpendingService} from "../spending/spending.service";
 import {User} from "../decarators/user.decarator";
-import {ChartDataset, ChartService} from "./chart.service";
+import {ChartService} from "./chart.service";
 import {AuthGuard} from "../guards/auth.guard";
 import {getChartDataDto} from "./dto/chart.dto";
 
@@ -10,29 +10,38 @@ import {getChartDataDto} from "./dto/chart.dto";
 export class ChartController {
     constructor(
         private spendingService: SpendingService,
-        private chartService : ChartService,
-        ) {}
+        private chartService: ChartService,
+    ) {
+    }
 
     @Get('/getChartData')
-    async getChartData(@User('_id') userId : string, @Query() queryParams : getChartDataDto): Promise<ChartDataset[] | null> {
+    async getChartData(@User('_id') userId: string, @Query() queryParams: getChartDataDto) {
 
         const currentYear = Number(queryParams.year ?? new Date().getFullYear())
+        const currentMonth = Number(queryParams.month ?? new Date().getMonth())
 
         const paramsForSearchSpending = {
             createdAt: {
-                $gte: new Date(currentYear, 1, 1),
-                $lt: new Date(currentYear, 12, 31)
+                $gte: new Date(currentYear, currentMonth ? currentMonth : 1, 1),
+                $lt: new Date(currentYear, currentMonth ? currentMonth : 12, 31)
             },
-            userId
+            userId,
+            walletId: queryParams.walletId
         }
-
         const allHistory = await this.spendingService.getSpendingByParameters(paramsForSearchSpending);
         if (!allHistory) {
             throw new HttpException('userId not correct', HttpStatus.BAD_REQUEST);
         }
-        const dataSetForChart = await this.chartService.getChartDataset(allHistory)
-        return dataSetForChart
+        if (queryParams.isMobile) {
+            if (queryParams.typeChart === 'pie') {
+                return {
+                    chartData: await this.chartService.getChartDatasetForMobilePie(allHistory),
+                    date: paramsForSearchSpending.createdAt.$lt
+                }
+            }
+        } else {
+            return await this.chartService.getChartDataset(allHistory)
+        }
     }
-
 
 }
