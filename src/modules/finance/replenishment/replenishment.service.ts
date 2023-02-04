3c10592aed2b41b41444c3361bc162ce695b6f3b
Nick <shuminskiy.nik@mail.ru>
2023-02-04 17:04:49 +0300

    update server

---
+++ src/modules/finance/replenishment/replenishment.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from '@nestjs/common';
+import {InjectModel} from "@nestjs/mongoose";
+import {Model} from "mongoose";
+import {ReplenishmentModel} from "../../../models/replenishment.model";
+import {CreateReplenishmentDto, DeleteReplenishmentDto, GetReplenishmentDto} from "./dto/replenishment.dto";
+
+
+@Injectable()
+export class ReplenishmentService {
+
+    constructor(
+        @InjectModel(ReplenishmentModel.name) private replenishmentModel: Model<ReplenishmentModel>,
+    ) {
+    }
+
+    async getReplenishmentByWalletId({walletId, replenishmentId} : GetReplenishmentDto) : Promise<ReplenishmentModel | null> {
+        return this.replenishmentModel.findOne({walletId, _id : replenishmentId})
+    }
+
+    async updateReplenishment({replenishment, userId, walletId}: CreateReplenishmentDto): Promise<ReplenishmentModel | null> {
+        return this.replenishmentModel.findOneAndUpdate(
+            {
+                _id: replenishment._id,
+                userId,
+                walletId
+            }
+            ,
+            replenishment
+            ,
+            {
+                new: true,
+                overwrite: false
+            }
+        );
+    }
+
+    async addReplenishment({walletId, userId, replenishment}: CreateReplenishmentDto): Promise<ReplenishmentModel | null> {
+        return await this.replenishmentModel.create({...replenishment, userId, walletId})
+    }
+
+    async deleteReplenishment({walletId, userId, replenishmentId}: DeleteReplenishmentDto): Promise<ReplenishmentModel | null> {
+        return this.replenishmentModel.findOneAndDelete({_id: replenishmentId, walletId, userId});
+    }
+}
